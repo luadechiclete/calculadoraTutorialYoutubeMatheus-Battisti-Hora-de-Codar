ac0858899b476e572fffbf384bcfaa21f85c1806
Luana Santos <santosmeiraluana@gmail.com>
2022-08-10 19:57:43 -0300

    ao clicar no botão recebe o valor do texto dele.

--- Projeto/js/scripts.js
+++ Projeto/js/scripts.js
@@ -3,3 +3,17 @@ const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
 console.log(buttons);
+
+class Calculator {
+    
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        const value = e.target.innerText;
+
+        console.log(value);
+
+    })
+})
